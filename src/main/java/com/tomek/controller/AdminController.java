ed1aa8e9a38bb620e9f484fd891eeff6160b5ea3
ZiebaTomasz <ziebatomasz3@gmail.com>
2018-03-09 14:24:30 +0100

    Edit, add medicines and security config

--- src/main/java/com/tomek/controller/AdminController.java
+++ src/main/java/com/tomek/controller/AdminController.java
@@ -2,13 +2,14 @@ package com.tomek.controller;
 
 import com.tomek.domain.Medicine;
 import com.tomek.service.MedicineService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
@@ -21,21 +22,25 @@ public class AdminController {
   }
 
 
-  @RequestMapping("/admin/edit/{id}")
+  @GetMapping("/admin/edit/{id}")
   public String editMedicine(@PathVariable (value = "id")Long id, Model model) {
-      return "";
+    model.addAttribute("medicine",medicineService.medicineById(id));
+      return "admin/editMedicine";
   }
 
-  @PostMapping("admin/medicines")
-  public String saveMedicine(Medicine medicine){
+  @PostMapping("admin/save")
+  public String saveMedicine(@Valid Medicine medicine, BindingResult bindingResult){
+    if (bindingResult.hasErrors()){
+      System.out.println("some error");
+    }
     medicineService.save(medicine);
     return "redirect:/";
   }
 
-  @RequestMapping("/admin/create")
-  public String create(Model model) {
+  @GetMapping("/admin/create")
+  public String createMedicine(Model model) {
     model.addAttribute("medicine", new Medicine());
-    return "editMedicine";
+    return "admin/addMedicine";
   }
 
   @GetMapping(value = "/admin/delete/{id}")
